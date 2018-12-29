30325532ff1246e29d2b8ad42ae6e586e81238a3
vasile <elsile69@yahoo.com>
2018-12-29 22:22:39 +0200

    new installer[incomplete]

---
+++ plug.sh
@@ -0,0 +1,62 @@
+set -euC
+IFS=$'\n'
+
+if ! [[ -d pack/bundle/start && -d pack/bundle/opt ]]; then
+  mkdir -p pack/bundle/start pack/bundle/opt
+fi
+
+cd pack/bundle/start/
+
+start=(
+  "sgur/vim-editorconfig"
+  "tpope/vim-fugitive"
+  "cohama/agit.vim"
+  "tpope/vim-vinegar"
+  "markonm/traces.vim"
+  "whiteinge/diffconflicts"
+  "haya14busa/vim-asterisk"
+  "AndrewRadev/inline_edit.vim"
+  "tpope/vim-dispatch"
+  "basilgood/agrep"
+  "djoshea/vim-autoread"
+  "hauleth/asyncdo.vim"
+  "ctrlpvim/ctrlp.vim"
+  "vim-scripts/sessionman.vim"
+  "Shougo/neocomplete"
+)
+
+for element in "${start[@]}"; do
+  if ! [ -d "${element#*/}" ]; then
+    echo "cloning '$element'"
+    git clone --quiet https://github.com/$element.git &
+  fi
+done
+
+cd ../opt/
+
+opt=(
+  "w0rp/ale"
+  "thinca/vim-quickrun"
+  "kana/vim-repeat"
+  "tpope/vim-commentary"
+  "tpope/vim-surround"
+  "mileszs/ack.vim"
+  "PanagiotisS/LargeFile"
+  "neoclide/vim-jsx-improve"
+  "sgur/cmdline-completion"
+  "h1mesuke/vim-alignta"
+  "terryma/vim-expand-region"
+  "stefandtw/quickfix-reflector.vim"
+  "markonm/hlyank.vim"
+  "simnalamburt/vim-mundo"
+  "machakann/vim-swap"
+  "dyng/ctrlsf.vim"
+
+)
+
+for element in "${opt[@]}"; do
+  if ! [ -d "${element#*/}" ]; then
+    echo "cloning '$element'"
+    git clone --quiet https://github.com/$element.git &
+  fi
+done
